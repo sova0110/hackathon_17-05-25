56cbd2307b32106826cbbb58752e9d066b12e78f
Irina <irinaarutkina606@gmail.com>
2025-05-17 15:39:17 +0300

    скрипт для рекомендаций

--- student.js
+++ student.js
@@ -1,6 +1,3 @@
 const studentModal = document.getElementById("student__modal");
 const openModal = document.getElementById("button__modal__rec");
 const closeModal = document.getElementById("");
-
-openModal.addEventListener("click", async () => {
-
