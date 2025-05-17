51e68ab693c00a449bb57a2eef49b22ec1999e0e
Irina <irinaarutkina606@gmail.com>
2025-05-17 14:51:52 +0300

    Сделала модальное окно

---
+++ student.js
@@ -0,0 +1,8 @@
+const studentModal = document.getElementById("student__modal");
+const openModal = document.getElementById("button__modal__rec");
+const closeModal = document.getElementById("");
+
+openModal.addEventListener("click", () => {
+    studentModal.style.display = "grid";
+});
+
